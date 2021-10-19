16d779abce091965b7547718ac65e541b99b3325
Maxim Matous <maximmatous@seznam.cz>
2021-10-19 19:32:19 +0200

    Přidejte soubory projektu.

---
+++ DU_vetsi_mensi/DU_vetsi_mensi.cpp
@@ -0,0 +1,28 @@
+﻿//porovnání jaké číslo ze dvou zadaných je větší
+
+//možno bude třeba do definice postprocesoru přidat: _CRT_SECURE_NO_WARNINGS
+//microsoft vyhodnucuje funkci scanf() jako bezpečnostní riziko
+
+#include <stdio.h>
+
+int main(void) {
+	int a;
+	int b;
+
+	printf("Zadejte cislo a: ");
+	scanf("%d", &a);
+
+	printf("Zadejte cislo b: ");
+	scanf("%d", &b);
+
+	printf("\nCislo %d je ", a);
+
+	if (a < b) {
+		printf("mensi");
+	}
+	else {
+		printf("vetsi");
+	}
+
+	printf(" nez cislo %d\n", b);
+}
